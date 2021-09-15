5f3f36de27bd553f3bf5a4fa13c651c2261da573
Shawan Mandal <shawan.sm049@gmail.com>
2021-09-15 16:42:38 +0530

    Update headers

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 
 const headers = { 
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
-    'Content-Type' : 'application/json' 
+    'Content-Type' : 'application/x-www-form-urlencoded' 
  };
 
 /* GET scraper. */
